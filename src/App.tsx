dc4430f5ff6c6175e04ae6bb51dc80b832c0dac0
Dongho Seo <ehcws333@gmail.com>
2023-05-28 16:22:55 +0900

    chore: delete canvas postion and fov

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ScrollControls } from "@react-three/drei";
 function App() {
   return (
     <>
-      <Canvas postion={[0, 0, 5]} fov={30}>
+      <Canvas>
         <color attach="background" args={["#ececec"]} />
         <ScrollControls pages={80} damping={0.3}>
           <Experience />
